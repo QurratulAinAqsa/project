55b45f5d8a147429bc888a7b68ce7b51dd0fb177
QurratulAinAqsa <qurratulainaqsa@gmail.com>
2024-10-11 18:01:31 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ from tkinter import Tk, Entry, Button, StringVar
 class Calculator:
     def __init__(self,master):
         master.title("Calculator")
-        master.geometry('357x420+0+0')
+        master.geometry('420x420+0+0')
         master.config(bg='gray')
         master.resizable(False,False)
 
